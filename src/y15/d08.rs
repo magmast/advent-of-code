71a92588de6e4aacec410f632827c9d82c8b35c0
Maciej Augustyniak <maciej@augustyniak.app>
2025-03-21 06:52:29 +0100

    feat: add y15 d08 p2

--- src/y15/d08.rs
+++ src/y15/d08.rs
@@ -2,11 +2,10 @@ use anyhow::Result;
 
 mod parser {
     use nom::{
-        AsChar, Compare, IResult, Input, Parser,
+        AsChar, IResult, Input, Offset, Parser,
         branch::alt,
-        bytes::complete::tag,
         character::complete::{char, none_of, one_of, satisfy},
-        combinator::map,
+        combinator::{map, recognize},
         error::ParseError,
         multi::fold_many1,
         sequence::{delimited, preceded},
@@ -39,12 +38,12 @@ mod parser {
 
     pub fn string<I, E>(input: I) -> IResult<I, String, E>
     where
-        I: Input + Compare<&'static str>,
+        I: Input,
         <I as Input>::Item: AsChar,
         E: ParseError<I>,
     {
         delimited(
-            tag("\""),
+            char('"'),
             fold_many1(
                 alt((escaped_char, none_of("\""))),
                 || String::new(),
@@ -53,9 +52,90 @@ mod parser {
                     acc
                 },
             ),
-            tag("\""),
+            char('"'),
         )
         .parse(input)
+    }
+
+    pub trait AsStr {
+        fn as_str(&self) -> &str;
+    }
+
+    impl AsStr for &str {
+        fn as_str(&self) -> &str {
+            self
+        }
+    }
+
+    impl AsStr for &[u8] {
+        fn as_str(&self) -> &str {
+            std::str::from_utf8(self).unwrap()
+        }
+    }
+
+    fn doublescaped_char<I, E>(input: I) -> IResult<I, String, E>
+    where
+        I: Input + Offset + AsStr,
+        <I as Input>::Item: AsChar,
+        E: ParseError<I>,
+    {
+        map(
+            preceded(
+                char('\\'),
+                alt((
+                    map(one_of(r#"\""#), |ch| format!(r"\{}", ch)),
+                    map(recognize((char('x'), hexdigit, hexdigit)), |i: I| {
+                        i.as_str().to_string()
+                    }),
+                )),
+            ),
+            |s| format!(r"\\{}", s),
+        )
+        .parse(input)
+    }
+
+    pub fn doublescaped<I, E>(input: I) -> IResult<I, String, E>
+    where
+        I: Input + Offset + AsStr,
+        <I as Input>::Item: AsChar,
+        E: ParseError<I>,
+    {
+        map(
+            delimited(
+                char('"'),
+                fold_many1(
+                    alt((
+                        doublescaped_char,
+                        map(none_of("\""), |ch| format!("{}", ch)),
+                    )),
+                    || String::new(),
+                    |mut acc, s| {
+                        acc += &s;
+                        acc
+                    },
+                ),
+                char('"'),
+            ),
+            |s| format!(r#""\"{}\"""#, s),
+        )
+        .parse(input)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_doublescaped_char() {
+            let (_, s) = doublescaped_char::<_, ()>(r#"\""#).unwrap();
+            assert_eq!(s, r#"\\\""#);
+
+            let (_, s) = doublescaped_char::<_, ()>(r#"\\"#).unwrap();
+            assert_eq!(s, r"\\\\");
+
+            let (_, s) = doublescaped_char::<_, ()>(r#"\x27"#).unwrap();
+            assert_eq!(s, r"\\x27");
+        }
     }
 }
 
@@ -64,21 +144,33 @@ fn escape(s: &str) -> String {
     escaped
 }
 
-pub async fn p1() -> Result<()> {
+fn doublescape(s: &str) -> String {
+    let (_, doublescaped) = parser::doublescaped::<_, ()>(s).unwrap();
+    doublescaped
+}
+
+async fn answer(mut parser: impl FnMut(&str) -> String) -> Result<()> {
     let input = tokio::fs::read_to_string("inputs/y15_d08.txt").await?;
     let escaped =
         input
             .lines()
-            .map(|line| escape(line.trim()))
+            .map(|line| parser(line.trim()))
             .fold(String::new(), |mut acc, line| {
                 acc.push_str(&line);
                 acc.push('\n');
                 acc
             });
-    println!("Answer: {}", input.len() - escaped.len());
+    println!(
+        "Answer: {}",
+        input.len().max(escaped.len()) - escaped.len().min(input.len())
+    );
     Ok(())
 }
 
+pub async fn p1() -> Result<()> {
+    answer(escape).await
+}
+
 pub async fn p2() -> Result<()> {
-    todo!()
+    answer(doublescape).await
 }
